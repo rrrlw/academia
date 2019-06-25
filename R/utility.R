b21e6c86f8c116d4bae84c819832b447066cf504
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-06-24 21:04:33 -0400

    Added function to read scholar names/ids

--- R/utility.R
+++ R/utility.R
@@ -1,5 +1,5 @@
 ## format names correctly
-name.val <- function(first, middle, last) {
+name_val <- function(first, middle, last) {
   if (is.na(middle)) {
     return(paste(capitalize(tolower(first)),
                  capitalize(tolower(last))))
@@ -10,3 +10,13 @@ name.val <- function(first, middle, last) {
   }
 }
 
+## read in academician info data
+read_scholars <- function(file_name) {
+  readr::read_csv(file = file_name,
+                  col_types = cols(
+                    First = col_character(),
+                    Middle = col_character(),
+                    Last = col_character(),
+                    ID = col_character()
+                  ))
+}
